f8cc1968334c5f00e70c74019686815940d9a87d
mddirga <madedirga22@gmail.com>
2024-09-27 15:08:44 +0800

    Feat: Map Display

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
     <title>Employee Attendance</title>
+
+    <style>
+        #map {
+            height: 400px;
+        }
+    </style>
 </head>
 <body>
     {{ $slot }}
